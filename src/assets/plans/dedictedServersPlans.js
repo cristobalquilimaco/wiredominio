bce2cec63d245a2bed1748c8b201dccd818db972
Cristobal <quilimacox1@gmail.com>
2024-11-23 18:52:43 -0500

    Se actualiza dedicated servers plans

--- src/assets/plans/dedictedServersPlans.js
+++ src/assets/plans/dedictedServersPlans.js
@@ -1,3 +1,41 @@
-const plans = [
-    
-]
+const dedicatedServer = [
+    {
+      id: 1,
+      name: 'Dedicated Servers in Miami, FL - Small',
+      url: 'https://my.donhoster.com/cart.php?a=add&pid=2',
+      price: '$60 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['Intel Dual Xeon 3.0 Ghz', '4 GB RAM Memory', '500 GB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support', ],
+    },
+    {
+      id: 2,
+      name: 'Dedicated Servers in Miami, FL - Medium',
+      url: 'https://my.donhoster.com/cart.php?a=add&pid=8',
+      price: '$82 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['Intel Dual Xeon 3.0 Ghz', '8 GB RAM Memory', '1 TB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support',],
+    },
+    {
+      id: 3,
+      name: 'Dedicated Servers in Miami, FL - Large',
+      url: 'https://my.donhoster.com/cart.php?a=add&pid=9',
+      price: '$120 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['Intel Dual Xeon 3.0 Ghz', '16 GB RAM memory', '1TB HHD 1', 'IP (IPv4)', 'Root access / Remote Desktop', '24/7 Support',],
+    },
+    {
+      id: 4,
+      name: 'Dedicated Servers in Miami, FL - Extra Large',
+      url: 'https://my.donhoster.com/cart.php?a=add&pid=10',
+      price: '$220 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['Intel Dual Xeon 3.0 Ghz', '32 GB RAM Memory', '2x1 TB HHD', '1 IP (IPv4)', 'Root access / Remote Desktop', '24/7 support',],
+    },
+
+  ];
+
+  export default dedicatedServer
