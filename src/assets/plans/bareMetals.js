0a2e7da478925c79b7455a27402b13dffd5c06af
Cristobal <quilimacox1@gmail.com>
2024-11-27 18:31:08 -0500

    Se inicia codigo con arreglo de baremetals

---
+++ src/assets/plans/bareMetals.js
@@ -0,0 +1,40 @@
+const bareMetalServer = [
+    {
+    id: 1,
+    name: 'Miami - Dell PowerEdge R210',
+    url: 'https://my.donhoster.com/cart.php?a=add&pid=14',
+    price: '$95 USD',
+    savePrice: '$9.99',
+    savePercentage: 'Save 90%',
+    features: ['Bare Metal Dedicated Server in Miami', 'Intel Xeon X3400 processor families X3450', 'Up to 32GB DDR3 1066MHz memory support', '2 x 3.5" SATA/SSD drive options', 'Bare Metal chassis'],
+  },
+  {
+    id: 2,
+    name: 'US/East Coast - Supermicro MicroCloud',
+    url: 'https://my.donhoster.com/cart.php?a=add&pid=11',
+    price: '$120.00 USD',
+    savePrice: '$9.99',
+    savePercentage: 'Save 90%',
+    features: ['Bare Metal Dedicated Server in USA', 'Intel Xeon E3 processor families E3-1230 v3, E3-1240 v3, E3-1270 v3', 'Up to 32GB DDR3 1600MHz memory support', '2 x 3.5" SATA/SSD drive options', 'Fully Hotswappable node chassis', 'Redundant Power Supplies', ],
+  },
+  {
+    id: 3,
+    name: 'Miami - Dell PowerEdge R610',
+    url: 'https://my.donhoster.com/cart.php?a=add&pid=12',
+    price: '$156 USD',
+    savePrice: '$9.99',
+    savePercentage: 'Save 90%',
+    features: ['Bare Metal Dedicated Server in Miami', 'Two Intel Xeon 5600 processor families L5639', 'Up to 128GB DDR3 1600MHz memory support', '6 x 2.5" SATA/SSD drive options', 'Bare Metal chassis', 'Optional Redundant power supplies', ],
+  },
+  {
+    id: 4,
+    name: 'US/East Coast - Supermicro FatTwin',
+    url: 'https://my.donhoster.com/cart.php?a=add&pid=13',
+    price: '$178 USD',
+    savePrice: '$9.99',
+    savePercentage: 'Save 90%',
+    features: ['Bare Metal Dedicated Server in USA', 'Two Intel Xeon Hex-core processors E5-2620, E5-2620 v2', 'Up to 512GB DDR3 1600MHz memory support', '6 x 2.5" SATA/SAS/SSD drive bays', 'Fully Hotswappable node chassis', 'Redundant Power Supplies', ],
+  },
+  
+
+];
