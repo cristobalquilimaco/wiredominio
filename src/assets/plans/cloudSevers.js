89c899ecbb83e27bdfa899593b23d88c16cb60fa
Cristobal <quilimacox1@gmail.com>
2024-11-27 18:37:32 -0500

    Se ajusta cloud servers

---
+++ src/assets/plans/cloudSevers.js
@@ -0,0 +1,12 @@
+const cloudServer = [
+    {
+      id: 1,
+      name: 'Cloud VM',
+      url: 'https://my.donhoster.com/cart.php?a=add&pid=7',
+      price: '$20.05 USD',
+      savePrice: '$9.99',
+      savePercentage: 'Save 90%',
+      features: ['KVM Based Cloud Server in Miami', 'Includes industry leading server control panel', 'Intel Xeon processor families', 'Up to 32GB memory support', 'Up to 2TB SAN storage', 'Cloud Hosted Infrastructure', 'Redundant Power & Connectivity',],
+    },
+
+  ];
