56333ef9df740dae0a9dcf61790fd47c506758b5
Cristobal <quilimacox1@gmail.com>
2024-12-08 19:32:57 -0500

    Se ajusta seccion de preguntas frecuentes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,6 @@ const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach(faq =>{
     faq.addEventListener("click" , ()=>{
-        faq.classList.toggle(".active")
+        faq.classList.toggle("active")
     })
 })
